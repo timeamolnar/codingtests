5aebd3b2e3ae9ffac3a7c8c1af66d4e85d4c8da5
timeamolnar <timea.molnar3@gmail.com>
2015-11-23 12:45:51 +0000

    First solution for Test 4 added

--- src/main/java/codingtests/test4_listrange/ListRange.java
+++ src/main/java/codingtests/test4_listrange/ListRange.java
@@ -1,11 +1,20 @@
 package codingtests.test4_listrange;
 
+import com.google.common.base.Preconditions;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ListRange {
 
     public List<Integer> createList(int start, int end) {
-        return null;
+        Preconditions.checkArgument(start<=end,"Start should be smaller or equal to end");
+
+        List myList = new ArrayList<Integer>();
+        for (int i = start; i <= end; i++) {
+            myList.add(i);
+        }
+        return myList;
     }
 }
--- src/test/java/codingtests/test4_listrange/ListRangeTest.java
+++ src/test/java/codingtests/test4_listrange/ListRangeTest.java
@@ -9,7 +9,22 @@ import static org.junit.Assert.*;
 public class ListRangeTest {
     @Test
     public void range11ShouldGiveListWithOnly1(){
-        assertEquals(Arrays.asList(1),new ListRange().createList(1,1));
+        assertEquals(Arrays.asList(1), new ListRange().createList(1,1));
+    }
+
+    @Test
+    public void range12ShouldGiveListWith12(){
+        assertEquals(Arrays.asList(1,2), new ListRange().createList(1,2));
+    }
+
+    @Test
+    public void range13ShouldGiveListWith123(){
+        assertEquals(Arrays.asList(1,2,3), new ListRange().createList(1,3));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void startBiggerThanEndShouldBeInvalid(){
+        new ListRange().createList(1,0);
     }
 
 }
