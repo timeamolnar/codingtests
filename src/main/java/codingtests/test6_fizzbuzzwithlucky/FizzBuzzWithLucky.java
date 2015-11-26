d05aab04603f7d00c351cf6d06daddacd6ddfb29
timeamolnar <timea.molnar3@gmail.com>
2015-11-26 16:36:11 +0000

    Added solution for Test 6

--- src/main/java/codingtests/test6_fizzbuzzwithlucky/FizzBuzzWithLucky.java
+++ src/main/java/codingtests/test6_fizzbuzzwithlucky/FizzBuzzWithLucky.java
@@ -1,8 +1,45 @@
 package codingtests.test6_fizzbuzzwithlucky;
 
+import com.google.common.base.Preconditions;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzzWithLucky {
 
-    public String convertNumber(int i) {
-        return "lucky";
+    public static void main(String... arguments) {
+        FizzBuzzWithLucky fb = new FizzBuzzWithLucky();
+        fb.checkCommandLineArguments(arguments);
+        fb.convertNumberAndOut(Integer.valueOf(arguments[0]), System.out);
+    }
+
+    public void checkCommandLineArguments(String... arguments){
+        Preconditions.checkArgument(arguments.length == 1, "Program must get exactly one argument");
+        try{
+            Integer.valueOf(arguments[0]);
+        } catch (NumberFormatException ex){
+            throw new IllegalArgumentException("Argument must be a valid number");
+        }
+    }
+
+    public void convertNumberAndOut(int upperBound, PrintStream out){
+        getFizzBuzzValuesForNumbers(1,upperBound).stream().forEach(out::println);
+    }
+
+    public List<String> getFizzBuzzValuesForNumbers(int firstNumber, int lastNumber){
+        return IntStream.range(firstNumber,lastNumber+1).mapToObj(this::convertNumber).collect(Collectors.toList());
+    }
+
+    public String convertNumber(int nr) {
+        Preconditions.checkArgument(nr > 0,"Number should be greater than 0!");
+
+        if (String.valueOf(nr).contains("3")) return "lucky";
+        if (nr % 15 == 0) return "fizzbuzz";
+        if (nr % 3 == 0) return "fizz";
+        if (nr % 5 == 0) return "buzz";
+
+        return String.valueOf(nr);
     }
 }
--- src/test/java/codingtests/test6_fizzbuzzwithlucky/FizzBuzzWithLuckyTest.java
+++ src/test/java/codingtests/test6_fizzbuzzwithlucky/FizzBuzzWithLuckyTest.java
@@ -6,9 +6,60 @@ import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzWithLuckyTest {
     private final FizzBuzzWithLucky fb = new FizzBuzzWithLucky();
+
+    //argTests
+    @Test(expected = IllegalArgumentException.class)
+    public void argumentCheckShouldBeInvalidForMoreThanOneArg(){
+        fb.checkCommandLineArguments(new String[]{"1","2"});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void argumentCheckShouldBeInvalidForNotANumberArg(){
+        fb.checkCommandLineArguments(new String[]{"fifteen"});
+    }
+
     @Test
-    public void numberThreeShouldReturnLucky(){
+    public void argumentCheckShouldBeValidForANumberArg(){
+        fb.checkCommandLineArguments(new String[]{"14"});
+        // no exception
+    }
+
+    //logicTests
+    @Test
+    public void numberContainingThreeShouldReturnLucky(){
+        assertEquals("lucky", fb.convertNumber(13));
         assertEquals("lucky", fb.convertNumber(3));
+        assertEquals("lucky", fb.convertNumber(1113));
+        assertEquals("lucky", fb.convertNumber(333));
+        assertEquals("lucky", fb.convertNumber(35));
+    }
+
+    @Test
+    public void numberDividableByThreeButNotByFiveAndNotContainingThreeShouldReturnFizz(){
+        assertEquals("fizz", fb.convertNumber(21));
+        assertEquals("fizz", fb.convertNumber(66));
+        assertEquals("fizz", fb.convertNumber(96));
+    }
+
+    @Test
+    public void numberDividableByFiveButNotByThreeAndNotContainingThreeShouldReturnBuzz(){
+        assertEquals("buzz", fb.convertNumber(5));
+        assertEquals("buzz", fb.convertNumber(50));
+        assertEquals("buzz", fb.convertNumber(22250));
+    }
+
+    @Test
+    public void numberFifteenAndNotContainingThreeShouldReturnFizzBuzz() {
+        assertEquals("fizzbuzz", fb.convertNumber(15));
+        assertEquals("fizzbuzz", fb.convertNumber(1515));
+    }
+
+    @Test
+    public void numberNotDividableByThreeorFiveAndNotContainingThreeShouldReturnItself() {
+        assertEquals("41", fb.convertNumber(41));
+        assertEquals("22", fb.convertNumber(22));
+        assertEquals("11", fb.convertNumber(11));
+        assertEquals("547", fb.convertNumber(547));
     }
 
 }
